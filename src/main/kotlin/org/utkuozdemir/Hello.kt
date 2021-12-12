c0e05187dc4aaa42b2c859f830785208970eae58
Utku Ozdemir <uoz@protonmail.com>
2021-12-13 00:02:03 +0100

    add sources

---
+++ src/main/kotlin/org/utkuozdemir/Hello.kt
@@ -0,0 +1,5 @@
+package org.utkuozdemir
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
---
+++ src/test/kotlin/org/utkuozdemir/HelloTest.kt
@@ -0,0 +1,12 @@
+package org.utkuozdemir
+
+import org.junit.jupiter.api.Test
+
+
+class HelloTest {
+
+    @Test
+    fun testHello() {
+        println("Hello from test!")
+    }
+}
