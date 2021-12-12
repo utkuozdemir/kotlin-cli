8f08808ebe179936270bd2b4b6474958b3a49186
Utku Ozdemir <uoz@protonmail.com>
2021-12-13 00:18:39 +0100

    simplify, add maven wrapper

--- src/main/kotlin/org/utkuozdemir/Hello.kt
+++ src/main/kotlin/org/utkuozdemir/Hello.kt
@@ -1,5 +1,20 @@
 package org.utkuozdemir
 
-fun main(args: Array<String>) {
+import java.nio.file.Files
+
+fun main() {
     println("Hello, World")
+
+    val tempFile = Files.createTempFile("kotlin-cli", ".txt")
+    println("Created temp file $tempFile")
+    try {
+        Files.write(tempFile, "hello file!".toByteArray())
+        println("Wrote to file")
+        val fileContent = Files.readAllBytes(tempFile).decodeToString()
+        println("Read from file: $fileContent")
+
+    } finally {
+        Files.delete(tempFile)
+        println("Deleted file $tempFile")
+    }
 }
