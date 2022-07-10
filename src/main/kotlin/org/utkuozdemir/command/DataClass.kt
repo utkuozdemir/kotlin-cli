5b72a53abaa987e6e30027aa5aa567fa99efa3bf
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2022-07-10 16:45:58 +0200

    chore: fix build & improvements

--- src/main/kotlin/org/utkuozdemir/command/DataClass.kt
+++ src/main/kotlin/org/utkuozdemir/command/DataClass.kt
@@ -2,11 +2,14 @@ package org.utkuozdemir.command
 
 import com.github.ajalt.clikt.core.CliktCommand
 
+private const val AGE_BEFORE = 42
+private const val AGE_AFTER = 52
+
 class DataClass : CliktCommand() {
     override fun run() {
-        val person1 = Person("Faik", 42)
+        val person1 = Person("Faik", AGE_BEFORE)
         println(person1)
-        val person2 = person1.copy(age = 52)
+        val person2 = person1.copy(age = AGE_AFTER)
         println(person2)
     }
 }
